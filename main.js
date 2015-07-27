23499d5e70cd0aad292a7dd9dc7d2cf7d8401b9b
Matheus Avellar <matheusavellarb@gmail.com>
2015-07-26 23:32:30 -0300

    Changed bugged stuff

--- main.js
+++ main.js
@@ -11,6 +11,8 @@
 
 "use strict";
 
+// v 0.0.4 //
+
 const PS_PATH = "https://rawgit.com/MatheusAvellar/plugSlack/master/resources/";
 
 var ps = {
@@ -24,33 +26,34 @@ var ps = {
         );
 
         $("div#header-panel-bar").append(
-            "<div id='slack-button' class='header-panel-button notify'>"
+            "<div id='ps-button' class='header-panel-button notify'>"
             +    "<i class='icon icon-star-white'></i>"
             +    "<span class='request-count'>0</span>"
             +"</div>"
         );
 
         $(".app-right").append(
-            "<div id='slack-chat'>"
-            +    "<div id='slack-header'>"
-            +        "<div id='channels-button' class='slack-button'>channels</div>"
-            +        "<div id='users-button' class='slack-button selected'>users</div>"
+            "<div id='ps-chat'>"
+            +    "<div id='ps-header'>"
+            +        "<div id='ps-channels-button' class='ps-btn'>channels</div>"
+            +        "<div id='ps-users-button' class='ps-btn selected'>users</div>"
             +    "</div>"
             +    "<div class='users-list'></div>"
             +"</div>"
         );
 
         $("div#chat-button, div#users-button, div#waitlist-button, div#friends-button").on("click", function() {
-            $("div#slack-button").removeClass("selected");
-            $("#slack-chat").hide();
+            $("div#ps-button").removeClass("selected");
+            $("#ps-chat").hide();
         });
 
-        $("div#slack-button").on("click", function() {
+        $("div#ps-button").on("click", function() {
             $(".header-panel-button").removeClass("selected");
             $("#chat, #user-lists, #waitlist, div.app-right div.friends").hide();
-            $("div#slack-button").addClass("selected");
+            $("div#ps-button").addClass("selected");
         });
-
+    },
+    start: function() {
         var slackWS;
         $.ajax({
             type: "GET",
@@ -83,7 +86,7 @@ var ps = {
     },
     utils: {
         appendUser: function(username, imgURL) {
-            $("div#slack-chat div.users-list").append(
+            $("div#ps-chat div.users-list").append(
                 "<div class='user'>"
                 +    "<div class='img' style='background: url(" + imgURL + ")'></div>"
                 +    "<div class='username'>" + username + "</span>"
