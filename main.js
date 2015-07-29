e2faf5109d26774006b2474000c145f4b301704b
Matheus Avellar <matheusavellarb@gmail.com>
2015-07-29 14:56:03 -0300

    Updates

--- main.js
+++ main.js
@@ -11,9 +11,9 @@
 
 "use strict";
 
-const version = "v0.0.34";
+const version = "v0.0.35";
 const PS_PATH = "https://rawgit.com/MatheusAvellar/plugSlack/master/resources/";
-var ps, slackObj, tkn, cn;
+var ps, slackObj, slackWS, tkn, cn;
 var _all = {
     users: {},
     channels: {}
@@ -23,7 +23,6 @@ $.ajax({
 type: "GET",
 url: PS_PATH + "styles.css",
 success: function(_ajxData) {
-    //console.log("Successfully loaded the CSS");
     $("head").append(
         "<link "
         +    " rel='stylesheet' "
@@ -84,7 +83,6 @@ ps = {
         });
     },
     start: function() {
-        var slackWS;
         tkn = $("input#ps-token").val().trim();
         if (tkn) {
             $.ajax({
@@ -94,13 +92,9 @@ ps = {
                     $("div.ps-start").remove();
                     slackObj = data;
                     for (var i = 0, l = slackObj.users.length; i < l; i++) {
-                        console.log("A");
                         if (!slackObj.users[i].deleted) {
-                            console.log("B");
                             for (var j = 0, k = slackObj.ims.length; j < k; j++) {
-                                console.log("C");
                                 if (slackObj.users[i].id == slackObj.ims[j].user) {
-                                    console.log("D");
                                     ps.utils.appendItem(
                                         {
                                             name: slackObj.users[i].name,
@@ -114,11 +108,9 @@ ps = {
                                         name: slackObj.users[i].name,
                                         prof: slackObj.users[i].profile
                                     }
-                                    break
+                                    _all.channels[slackObj.ims[j].id] = slackObj.users[i].name;
+                                    break;
                                 } else if (slackObj.users[i].name == slackObj.self.name) {
-                                    console.log("----E----");
-                                    console.log(i);
-                                    console.log(slackObj.users[i].id);
                                     ps.utils.appendItem(
                                         {
                                             name: slackObj.users[i].name,
@@ -138,6 +130,14 @@ ps = {
                     }
                     $("div.users-list div.user[ps-uid^='USLACKBOT']").addClass("selected");
                     ps.utils.loadHistory(slackObj.ims[0].id);
+                    for (var i = 0, l = slackObj.bots.length; i < l; i++) {
+                        if (!slackObj.bots[i].deleted) {
+                            _all.users[slackObj.bots[i].id] = {
+                                name: slackObj.bots[i].name,
+                                prof: slackObj.bots[i].icons
+                            }
+                        }
+                    }
                     for (var i = 0, l = slackObj.channels.length; i < l; i++) {
                         if (!slackObj.channels[i].is_archived && slackObj.channels[i].is_member) {
                             ps.utils.appendItem(
@@ -171,76 +171,70 @@ ps = {
                             + "div.user.selected, "
                             + "div.group.selected").removeClass("selected");
                             $("div.ps-message").remove();
-                            $(this).addClass("selected");
+                            $("div.ps-nothing-here").remove();
+                            $(this).addClass("selected").removeClass("new");
                             cn = $(this).attr("ps-cid");
                             ps.utils.loadHistory(cn);
                         }
                     });
 
-                    console.log("Connecting account " + slackObj.self.name);
                     slackWS = new WebSocket(slackObj.url);
 
                     slackWS.onopen = function(_data){
-                        console.log("Successfully connected account " + slackObj.self.name);
-                        console.log(_data);
+                        console.log("Connected " + slackObj.self.name);
                     }
 
                     slackWS.onmessage = function(_data){
+                        _data = JSON.parse(_data.data);
+                        console.log("@ onmessage");
                         console.log(_data);
-                        var _d = JSON.parse(_data.data);
-                        if (_d.type == "message") {
-                            var d = new Date();
-                            var h = d.getHours();
-                            var m = d.getMinutes();
-                            if (h < 10) {  h = "0" + h;  }
-                            if (m < 10) {  m = "0" + m;  }
-                            var _u = _all.users[_d.user] ? _all.users[_d.user] : _all.users[_d.username];
-                            if (_all.users[_u]) {
+                        if (_data
+                         && _data.type == "message") {
+                            var current = {
+                                user: "",
+                                channel: ""
+                            };
+                            if (_data.user) {
+                                console.log(_all.users[_data.user].name);
+                                current.user = _all.users[_data.user].name;
+                            }
+                            if (_data.channel) {
+                                console.log(" @ " + _all.channels[_data.channel]);
+                                current.channel = _all.channels[_data.channel];
+                            }
+                            if (_data.channel == $("div.channel.selected").attr("ps-cid")) {
+                                var _p = _all.users[_data.user].prof;
+                                if (_p.image_32) {  _p = _p.image_32;  } else {  _p = _p.image_48;  }
                                 ps.utils.appendMessage(
                                     {
-                                        id: _u,
-                                        name: _all.users[_u].name,
-                                        prof: _all.users[_u].prof.image_32,
-                                        cid: _d.channel,
-                                        channel: _all.channels[_d.channel],
-                                        message: _d.text,
-                                        time: h + ":" + m
+                                        message: _data.text,
+                                        name: current.user,
+                                        id: _data.user,
+                                        prof: _p,
+                                        channel: current.channel,
+                                        cid: _data.channel,
+                                        time: ps.utils.fixTime(_data.ts)
                                     }
                                 );
-                            }
-                        } else if (_d.type == "presence_change" || _d.type == "manual_presence_change") {
-                            var _user = _d.user ? _d.user : slackObj.self.id;
-                            $("div.users-list div.user[ps-uid^='" + _user + "'] div.presence")
-                                .attr("class", "presence " + _d.presence);
-                        } else if (_d.type == "user_change") {
-                            if (!_d.user.deleted) {
-                                var _uid = _d.user.id;
-                                var _un = _d.user.name;
-                                _all.users[_uid] = _un;
-                                $("div.users-list div.user[ps-uid^='" + _uid + "'] div.username")
-                                    .text(_un);
-                            } else {
-                                var _uid = _d.user.id;
-                                //delete _all.users[_uid];
-                                $("div.users-list div.user[ps-uid^='" + _uid + "']")
-                                    .remove();
+                            } else if (_data.channel) {
+                                var _cid = _data.channel;
+                                var _l = _cid.trim()[0] == "D" ? "user" : _cid.trim()[0] == "C" ? "channel" : "group";
+                                $("div." + _l + "[ps-cid^='" + _cid + "']").addClass("new");
                             }
                         }
                     }
 
                     slackWS.onerror = function(_data) {
-                        console.log(_data);
+                        console.log("@ onerror");
                     }
                 },
                 error: function(data) {
                     console.log("Error authenticating");
                     console.log(data);
                 }
-            }).done(function(data) {
-                console.log("[Status: " + JSON.stringify(data.status) + "]");
             });
         } else {
-            console.log("Please insert a token");
+            console.log("Token not inserted");
         }
     },
     utils: {
@@ -268,9 +262,15 @@ ps = {
             }
         },
         appendMessage: function(obj) {
+            console.log("@ appendMessage");
             if (obj.message) {
-                obj.from = !obj.from ? "" : obj.from;
-                obj.message = obj.message.split("<").join("&lt;").split(">").join("&gt;");
+                console.log("@ appendMessage.success");
+                obj.name = !obj.name ? "" : obj.name;
+                obj.message = obj.message
+                            .split("<").join("&lt;")
+                            .split(">").join("&gt;")
+                            .split("!§").join("<")
+                            .split("§!").join(">");
                 var _c = $("div#ps-actual-chat")
                 var _scroll = _c[0].scrollTop > _c[0].scrollHeight -_c.height() - 28;
                 $("#ps-actual-chat").append(
@@ -297,31 +297,82 @@ ps = {
                     url: "https://slack.com/api/" + _l + ".history?token=" + tkn + "&channel=" + cid,
                     success: function(data) {
                         if (data.ok) {
-                            for (var i = data.messages.length - 1, l = -1; i > l; i--) {
-                                var _u = data.messages[i].user;
-                                if (_all.users[_u]) {
-                                    ps.utils.appendMessage(
-                                        {
-                                            id: _u,
-                                            name: _all.users[_u].name,
-                                            prof: _all.users[_u].prof.image_32,
-                                            cid: cid,
-                                            channel: _all.channels[cid],
-                                            message: data.messages[i].text,
-                                            time: data.messages[i].ts
-                                        }
-                                    );
-                                } else {
-                                    console.log("-=[Error @ 315]=-\n" + _ul);
+                            console.log("@ loadHistory.success");
+                            if (data.messages.length > 0) {
+                                for (var i = data.messages.length - 1, l = -1; i > l; i--) {
+                                    var _u = data.messages[i].user;
+                                    if (_all.users[_u]) {
+                                        console.log("Double loadHistory success");
+                                        var _p = _all.users[_u].prof.image_32 ? "image_32" : "image_48";
+                                        ps.utils.appendMessage(
+                                            {
+                                                id: _u,
+                                                name: _all.users[_u].name,
+                                                prof: _all.users[_u].prof[_p],
+                                                cid: cid,
+                                                channel: _all.channels[cid],
+                                                message: ps.utils.format(data.messages[i].text),
+                                                time: ps.utils.fixTime(data.messages[i].ts)
+                                            }
+                                        );
+                                    } else {
+                                        console.log("-=[Error @ 315]=- [" + _u + "]");
+                                        console.log(data.messages[i]);
+                                    }
                                 }
+                            } else {
+                                $("div#ps-actual-chat").append("<div class='ps-nothing-here'></div>");
                             }
                         }
                     },
                     error: function(data) {
-                        console.log(data);
+                        console.log("@ loadHistory.error");
                     }
                 });
             }
+        },
+        format: function(message) {
+            var _final = message;
+            var _formats = [
+                {c: "*", r: "b"},
+                {c: "_", r: "em"},
+                {c: "```", r: "pre class='ps-pre-big'"},
+                {c: "`", r: "pre class='ps-pre-small'"}
+            ];
+            for (var _i = 0; _i < _final.length; _i++) {
+                for (var i = 0, l = _formats.length; i < l; i++) {
+                    if (_final.indexOf(_formats[i].c) != -1) {
+                        var _message = _final.replace(_formats[i].c, "!§" + _formats[i].r + "§!");
+                        if (_message.indexOf(_formats[i].c) != -1) {
+                            _message = _message.replace(_formats[i].c, "!§/" + _formats[i].r + "§!");
+                            _final = _message;
+                        }
+                    }
+                    if (_final.indexOf("<http") != -1) {
+                        _message = _final.replace("<http", "!§a href='http");
+                        if (_message.indexOf(">") > _final.indexOf("<https")) {
+                            _message = _message.replace(">", "' target='_blank' class='ps-link'§!Link!§/a§!");
+                            _final = _message;
+                        }
+                    }
+                }
+            }
+            return _final;
+        },
+        fixTime: function(secs) {
+            var t = (new Date(1970, 0, 1));
+            t.setSeconds(secs);
+            /*var m = t.getMinutes() + t.getTimezoneOffset();
+            var h = t.getHours();
+            while (m >= 60) {
+                m = m - 60;
+                h++;
+            }
+            if (h > 23) {  h = h - 24;   }
+            if (h < 10) {  h = "0" + h;  }
+            if (m < 10) {  m = "0" + m;  }
+            return h + ":" + m;*/
+            return t.getHours() + ":" + t.getMinutes();
         }
     }
 };
